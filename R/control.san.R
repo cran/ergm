cecee4feaaffa995b13c355a44a1e75a46ccb425
David R. Hunter <dhunter@stat.psu.edu>
2010-04-20 00:00:00 +0000

    version 2.2-4

--- R/control.san.R
+++ R/control.san.R
@@ -7,9 +7,8 @@
 #
 #  Copyright 2010 the statnet development team
 ######################################################################
-control.san<-function(prop.weights="default",prop.args=NULL,
+control.san<-function(prop.weights="default", prop.args=NULL,
                       drop=FALSE,
-                      summarizestats=FALSE,
                       maxchanges=1000000){
   control<-list()
   for(arg in names(formals(sys.function())))
@@ -17,12 +16,4 @@ control.san<-function(prop.weights="default",prop.args=NULL,
   control
 }
 
-control.san.ergm<-function(prop.weights=NULL,prop.args=NULL,
-                           drop=FALSE,
-                           summarizestats=FALSE,
-                           maxchanges=1000000){
-  control<-list()
-  for(arg in names(formals(sys.function())))
-    control[[arg]]<-get(arg)
-  control
-}
+
